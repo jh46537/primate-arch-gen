60dfffd25793415e8af9d2b113198b1c51ec3098
Ali Mansoorshahi <kayvan.mansoor@gmail.com>
2024-02-13 11:08:25 -0600

    Pass which removes bad SSA Extract insert pairs.

--- llvm/lib/Target/Primate/PrimateExtMerge.cpp
+++ llvm/lib/Target/Primate/PrimateExtMerge.cpp
@@ -1,14 +1,63 @@
 
 #include "PrimateExtMerge.h"
+#include "Primate.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
 
 namespace llvm {
 
+// Goal of this pass: SSA generates extra extracts and inserts
+// IF between an extract and an insert there is no operation, 
+// Then we can delete both of them, our data is mutable.
+
 bool PrimateExtMerge::runOnMachineFunction(MachineFunction& MF) {
+    dbgs() << "hello from Primate Extract Merger\n";
+    SmallVector<MachineInstr*> worklist; 
+    SmallVector<MachineInstr*> MIToRemove;
+    MachineRegisterInfo &MRI = MF.getRegInfo();
+    assert(MRI.isSSA() && "Not in SSA for merging aggregate ext/ins");
     for(MachineBasicBlock &MBB : MF) {
+        worklist.clear();
+        MIToRemove.clear(); 
         for(MachineInstr &MI: MBB) {
             MI.dump();
+            if(MI.getOpcode() == Primate::INSERT) {
+                worklist.push_back(&MI);
+            }
         }
+        for(auto* MI : worklist) {
+            dbgs() << "------\n";
+            MI->dump();
+            Register defReg = MI->getOperand(0).getReg();
+            Register wideInReg = MI->getOperand(1).getReg();
+            Register valueOp = MI->getOperand(2).getReg();
+            dbgs() << "Operand 1 is: "; MI->getOperand(2).dump();
+            assert(Register::isVirtualRegister(valueOp) && "op 1 is not a v-reg");
+            dbgs() << "Users for reg "<< Register::virtReg2Index(valueOp)  << ": \n";
+
+            // check if def comes from ext.
+            MachineInstr* defInstr = MRI.getVRegDef(valueOp);
+            assert(defInstr && "defining instruction doesnt exist for op 1 of insert...");
+            if(defInstr->getOpcode() == Primate::EXTRACT) {
+                defInstr->dump();
+                MIToRemove.push_back(defInstr);
+                MIToRemove.push_back(MI);
+                for(auto& userOP: MRI.use_operands(defReg)) {
+                    userOP.setReg(wideInReg);
+                }
+            }
+
+            // if def came from extract we will remove the insert, replace the use of the ins with the beegboiii
+
+            dbgs() << "------\n";
+        }
+
+        dbgs() << "removing ops\n";
+        for(auto* MI : MIToRemove) {
+            MI->eraseFromParent();
+        }
+
     }
+
     return false;
 }
 
