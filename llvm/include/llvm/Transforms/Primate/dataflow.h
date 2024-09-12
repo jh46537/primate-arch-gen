35b154e337429a87840a7ae8e734b1ab24a43f16
Andrew Nguyen <andrewnguyen@utexas.edu>
2024-09-12 16:01:41 -0500

    Refactored Arch Gen source files Dealing with an error in /llvm/include/llvm/IR/PassManager.h:580 ``` error: call to deleted constructor of 'llvm::PrimateArchGen'         new PassModelT(std::forward<PassT>(Pass))));                        ^~~~~~~~~~~~~~~~~~~~~~~~~ /work/anguyen/primate/primate-compiler/llvm/lib/Passes/PassRegistry.def:44:1: note: in instantiation of function template specialization 'llvm::PassManager<llvm::Module>::addPass<llvm::PrimateArchGen>' requested here MODULE_PASS("primate-arch-gen", PrimateArchGen()) ^ /work/anguyen/primate/primate-compiler/llvm/lib/Passes/PassBuilder.cpp:1469:9: note: expanded from macro 'MODULE_PASS'     MPM.addPass(CREATE_PASS); \         ^ /work/anguyen/primate/primate-compiler/llvm/include/llvm/Transforms/Primate/PrimateArchGen.h:41:5: note: 'PrimateArchGen' has been explicitly marked deleted here     PrimateArchGen(PrimateArchGen &&) = delete;     ^ /work/anguyen/primate/primate-compiler/llvm/include/llvm/IR/PassManagerInternal.h:71:28: note: passing argument to parameter 'Pass' here   explicit PassModel(PassT Pass) : Pass(std::move(Pass)) {} ```
    Changes to be committed:
          modified:   ../llvm/include/llvm/Transforms/Primate/PrimateArchGen.h
          modified:   ../llvm/include/llvm/Transforms/Primate/dataflow.h
          modified:   ../llvm/lib/Transforms/Primate/PrimateArchGen.cpp

--- llvm/include/llvm/Transforms/Primate/dataflow.h
+++ llvm/include/llvm/Transforms/Primate/dataflow.h
@@ -25,182 +25,194 @@ using namespace llvm;
 
 namespace {
 
-    typedef std::list<BasicBlock*> Worklist;
+typedef std::list<BasicBlock*> Worklist;
 
-    template <typename FlowValueType>			//FlowValueType indicates the type of flow value. Eg: BitVector
-        class DataFlow{
-            bool forward;						//flow direction; true for forward
-            public:
-            enum SetType {
-                IN,
-                OUT
-            };
+template <typename FlowValueType> // FlowValueType indicates the type of flow value. Eg: BitVector
+class DataFlow {
+    bool forward; // flow direction; true for forward
 
-            DataFlow(bool forward) {
-                this->forward = forward;  
-                in  = new BlockInOutMap();
-                out = new BlockInOutMap();
-                neighbourSpecificValues = new BlockInOutMap();
-                visited = new ValueMap<BasicBlock*, bool>();
+public:
+    enum SetType {
+        IN,
+        OUT
+    };
+
+    DataFlow(bool forward) {
+        this->forward = forward;  
+        in  = new BlockInOutMap();
+        out = new BlockInOutMap();
+        neighbourSpecificValues = new BlockInOutMap();
+        visited = new ValueMap<BasicBlock*, bool>();
+    }
+
+    virtual ~DataFlow(){
+        delete in;
+        delete out;
+        delete neighbourSpecificValues;
+        delete visited;
+     }
+    
+    //mapping from basicblock to lattice
+    typedef ValueMap<const BasicBlock*, FlowValueType*> BlockInOutMap;    
+    //IN and OUT sets for a basic block
+    BlockInOutMap *in;
+    BlockInOutMap *out;
+    BlockInOutMap *neighbourSpecificValues;
+    ValueMap<BasicBlock*, bool> *visited;
+
+    //forward and backward analysis functions
+    void performForwardAnalysis(Worklist &w) {
+        BasicBlock *hotBlock = *w.begin();
+        w.pop_front();
+        (*visited)[hotBlock] = true;
+        
+        //errs() << "Entering Block " << hotBlock->getName() << "\n";
+        
+        int numPred = 0;
+        for (pred_iterator PI = pred_begin(hotBlock), E = pred_end(hotBlock); PI != E; ++PI){
+            ++numPred;
+            if(PI == pred_begin(hotBlock)){
+                *(*in)[hotBlock] = *(*out)[*PI];
+            }else{
+                meetOp((*in)[hotBlock], (*out)[*PI]);
             }
+        }
 
-            virtual ~DataFlow(){
-                delete in;
-                delete out;
-                delete neighbourSpecificValues;
-                delete visited;
-             }
-            //mapping from basicblock to lattice
-            typedef ValueMap<const BasicBlock*, FlowValueType*> BlockInOutMap;    
-            //IN and OUT sets for a basic block
-            BlockInOutMap *in;
-            BlockInOutMap *out;
-            BlockInOutMap *neighbourSpecificValues;
-            ValueMap<BasicBlock*, bool> *visited;
-            
+        if(numPred == 0)  setBoundaryCondition((*in)[hotBlock]);
 
-            //forward and backward analysis functions
-            void performForwardAnalysis(Worklist &w){
-                BasicBlock *hotBlock = *w.begin();
-                w.pop_front();
-                (*visited)[hotBlock] = true;
-                
-                //errs() << "Entering Block " << hotBlock->getName() << "\n";
-                
-                int numPred = 0;
-                for (pred_iterator PI = pred_begin(hotBlock), E = pred_end(hotBlock); PI != E; ++PI){
-                    ++numPred;
-                    if(PI == pred_begin(hotBlock)){
-                        *(*in)[hotBlock] = *(*out)[*PI];
-                    }else{
-                        meetOp((*in)[hotBlock], (*out)[*PI]);
-                    }
-                }
+        FlowValueType *newOut = transferFn(*hotBlock);
+        bool changed = false;
+        changed = (*newOut != *(*out)[hotBlock]);
 
-                if(numPred == 0)  setBoundaryCondition((*in)[hotBlock]);
+        if(changed) {
+            *(*out)[hotBlock] = *newOut;
 
-
-                FlowValueType *newOut = transferFn(*hotBlock);
-                bool changed = false;
-                changed = (*newOut != *(*out)[hotBlock]);
-
-                if(changed) {
-                    *(*out)[hotBlock] = *newOut;
-
-                    for (succ_iterator SI = succ_begin(hotBlock), E = succ_end(hotBlock); SI != E; ++SI) {
-                        if(changed || !(*visited)[*SI]){                            
-                            w.push_back(*SI); 
-                        } 
-                    }
-                }
+            for (succ_iterator SI = succ_begin(hotBlock), E = succ_end(hotBlock); SI != E; ++SI) {
+                if(changed || !(*visited)[*SI]){                            
+                    w.push_back(*SI); 
+                } 
             }
+        }
+    }
 
-            void performBackwardAnalysis(Worklist &w){
-                BasicBlock *hotBlock = *w.begin();
-                w.pop_front();
-                (*visited)[hotBlock] = true;
-                
-                //errs() << "Entering Block " << hotBlock->getName() << "\n";
-                
-                //OUT of this basic block is equivalent to IN of its successor
-                int numSucc = 0; //to check for the exit node
+    void performBackwardAnalysis(Worklist &w) {
+        BasicBlock *hotBlock = *w.begin();
+        w.pop_front();
+        (*visited)[hotBlock] = true;
+        
+        //errs() << "Entering Block " << hotBlock->getName() << "\n";
+        
+        //OUT of this basic block is equivalent to IN of its successor
+        int numSucc = 0; //to check for the exit node
 
-                for (succ_iterator SI = succ_begin(hotBlock), E = succ_end(hotBlock); SI != E; ++SI) {
-                    numSucc++;
-                    if(SI == succ_begin(hotBlock)){
-                        *(*out)[hotBlock] = *(*in)[*SI]; //copy the first IN set values
-                    }else{
-                        meetOp((*out)[hotBlock], (*in)[*SI]); //call the meet operator                        
-                    }
-                }
-
-                if((*neighbourSpecificValues).find(hotBlock) != (*neighbourSpecificValues).end() ){
-                    //for phi node. meet the variables that are live from this specific block
-                    meetOp((*out)[hotBlock], (*neighbourSpecificValues)[hotBlock]);
-                }
-
-                if(numSucc == 0) setBoundaryCondition((*out)[hotBlock]); //set boundary condition for the exit node.
-
-                FlowValueType *newIn = transferFn(*hotBlock);
-
-                bool changed = false;
-                changed = ((*newIn) != (*(*in)[hotBlock]));
-
-                if(changed)
-                    *(*in)[hotBlock] = *newIn;
-                    
-                for (pred_iterator PI = pred_begin(hotBlock), E = pred_end(hotBlock); PI != E; ++PI) {
-                    if(changed || !(*visited)[*PI]){                            
-                        w.push_back(*PI); 
-                    } 
-                }
+        for (succ_iterator SI = succ_begin(hotBlock), 
+                            E = succ_end(hotBlock); SI != E; ++SI) {
+            numSucc++;
+            if (SI == succ_begin(hotBlock)) {
+                *(*out)[hotBlock] = *(*in)[*SI]; //copy the first IN set values
+            } else {
+                meetOp((*out)[hotBlock], (*in)[*SI]); //call the meet operator                        
             }
+        }
+
+        if ((*neighbourSpecificValues).find(hotBlock) != 
+            (*neighbourSpecificValues).end()) {
+            //for phi node. meet the variables that are live from this specific block
+            meetOp((*out)[hotBlock], (*neighbourSpecificValues)[hotBlock]);
+        }
+        
+        //set boundary condition for the exit node.
+        if (numSucc == 0) setBoundaryCondition((*out)[hotBlock]); 
+
+        FlowValueType *newIn = transferFn(*hotBlock);
+
+        bool changed = false;
+        changed = ((*newIn) != (*(*in)[hotBlock]));
+
+        if(changed) *(*in)[hotBlock] = *newIn;
             
-            void finalizeBackwardAnalysis(Function &func){
-                for (Function::iterator i = func.begin(), e = func.end(); i != e; ++i){
-                    BasicBlock *hotBlock = &*i;
-                    if((*neighbourSpecificValues).find(hotBlock) != (*neighbourSpecificValues).end()){
-                        for (succ_iterator SI = succ_begin(&*i), SE = succ_end(&*i); SI != SE; SI++){
-                            meetOp((*in)[*SI], (*neighbourSpecificValues)[hotBlock]);
-                        }                            
-                    }
-                }
+        for (pred_iterator PI = pred_begin(hotBlock), 
+                            E = pred_end(hotBlock); PI != E; ++PI) {
+            if(changed || !(*visited)[*PI]){                            
+                w.push_back(*PI); 
+            } 
+        }
+    }
+    
+    void finalizeBackwardAnalysis(Function &func) {
+        for (Function::iterator i = func.begin(), 
+                                e = func.end(); i != e; ++i) {
+            BasicBlock *hotBlock = &*i;
+            if ((*neighbourSpecificValues).find(hotBlock) != 
+                (*neighbourSpecificValues).end()) {
+                for (succ_iterator SI = succ_begin(&*i), 
+                                   SE = succ_end(&*i); SI != SE; SI++) {
+                    meetOp((*in)[*SI], (*neighbourSpecificValues)[hotBlock]);
+                }                            
             }
+        }
+    }
 
-            //add just the last block in case of backward analysis
-            void initializeWorklist(Function &func, Worklist &worklist){
-                if(forward){
-                    BasicBlock& entry = func.getEntryBlock();
-                    worklist.push_back(&entry);
-                    return;
-                }
+    //add just the last block in case of backward analysis
+    void initializeWorklist(Function &func, Worklist &worklist) {
+        if (forward) {
+            BasicBlock& entry = func.getEntryBlock();
+            worklist.push_back(&entry);
+            return;
+        }
 
-                for (Function::iterator i = func.begin(), e = func.end(); i != e; ++i){
-                    int numSucc = 0;
+        for (Function::iterator i = func.begin(), 
+                                e = func.end(); i != e; ++i) {
+            int numSucc = 0;
 
-                    for (succ_iterator SI = succ_begin(&*i), SE = succ_end(&*i); SI != SE; SI++)
-                        numSucc++;
+            for (succ_iterator SI = succ_begin(&*i), 
+                               SE = succ_end(&*i); SI != SE; SI++)
+                numSucc++;
 
-                    if(numSucc==0) worklist.push_back(&*i);
-                }
-            }
+            if (numSucc==0) worklist.push_back(&*i);
+        }
+    }
 
-            bool runOnFunction(Function &F){
-                visited = new ValueMap<BasicBlock*,bool>();
-                for (Function::iterator bi = F.begin(), be = F.end(); bi != be; bi++) {
-                    BasicBlock * bb = &*bi;
+    bool runOnFunction(Function &F) {
+        visited = new ValueMap<BasicBlock*,bool>();
+        for (Function::iterator bi = F.begin(), 
+                                be = F.end(); bi != be; bi++) {
+            BasicBlock *bb = &*bi;
 
-                    //set the appropriate values for forward and backward flow
-                    //(*in)[bb] -> bit vector::lattice for that block
-                    (*in)[bb] = initializeFlowValue(*bi, IN);
-                    (*out)[bb] = initializeFlowValue(*bi, OUT);
-                }
+            //set the appropriate values for forward and backward flow
+            //(*in)[bb] -> bit vector::lattice for that block
+            (*in)[bb]  = initializeFlowValue(*bi, IN);
+            (*out)[bb] = initializeFlowValue(*bi, OUT);
+        }
 
-                Worklist *worklist = new Worklist();
-                initializeWorklist(F,*worklist);
-                while(!worklist->empty()){
-                    if(forward)
-                        performForwardAnalysis(*worklist);
-                    else
-                        performBackwardAnalysis(*worklist);
-                }
-                finalizeBackwardAnalysis(F);
-                
-                // this should be an analysis pass, but regardless we did not change the IR
-                return false;
-            }
+        Worklist *worklist = new Worklist();
+        initializeWorklist(F,*worklist);
+        while (!worklist->empty()) {
+            if (forward)
+                performForwardAnalysis(*worklist);
+            else
+                performBackwardAnalysis(*worklist);
+        }
+        finalizeBackwardAnalysis(F);
+        
+        // this should be an analysis pass, but regardless we did not change the IR
+        return false;
+    }
 
-            protected:
-            
-            virtual void setBoundaryCondition(FlowValueType*) = 0;
-            
-            virtual void meetOp(FlowValueType* lhs, const FlowValueType* rhs) = 0;
-            
-            virtual FlowValueType* initializeFlowValue(BasicBlock& b, SetType setType) = 0;
-            
-            virtual FlowValueType* transferFn(BasicBlock& b) = 0;
-        };
-}
+protected:
+    virtual void 
+    setBoundaryCondition(FlowValueType*) = 0;
+    
+    virtual void 
+    meetOp(FlowValueType* lhs, const FlowValueType* rhs) = 0;
+    
+    virtual FlowValueType* 
+    initializeFlowValue(BasicBlock& b, SetType setType) = 0;
+    
+    virtual FlowValueType* transferFn(BasicBlock& b) = 0;
+};
+
+} // namespace
 
 #endif
 
